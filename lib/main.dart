30952cc810174e6e368dfdeff670b2855fa0e783
srije <srijen222@gmail.com>
2025-08-03 15:41:20 +0530

    updated version to 1.5.5

--- lib/main.dart
+++ lib/main.dart
@@ -361,7 +361,7 @@ List<String> dropdownid = anCourseIds;
 final List<String> depts = ["AN","BIO","BIOT","BITS","CE","CHE","CHEM","CS","ECE", "ECON","ECOM","EEE","FIN","GS","HSS","INSTR","IS","MAC","MATH","ME","MF","MGTS","MSE","MST","PHA","PHY",];
 final List<String> grades = ["A","A-","B","B-","C","C-","D","E","NC","CLR","GD"];
 final List<String> sems = ["1 - 1","1 - 2","2 - 1","2 - 2","PS 1","3 - 1","3 - 2","ST 1","4 - 1","4 - 2",];
-final List<String> degreelist = ["B1","B2","B3","B4","B5","B7","A1","A2","A3","A4","A5","A7","A8","A9","AA","AB","AC","AD"];
+final List<String> degreelist = ["B1","B2","B3","B4","B5","B7","A1","A2","A3","A4","A5","A7","A8","A9","AA","AB","AC","AD","AJ"];
 final List<String> campuslist = ["Pilani", "Goa", "Hyd"];
 
 class MyHomePage extends StatefulWidget {
